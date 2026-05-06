3ec38a88aa7faf8ca013892cda4b5aad84b56622
fm702-hash <fm702@hotmail.com>
2026-05-06 08:29:56 +0300

    Update main.jsx

--- main.jsx
+++ main.jsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
+// inline App to avoid src folder issue
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
